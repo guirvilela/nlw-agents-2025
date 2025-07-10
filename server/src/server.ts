582ac379936f749a69cbddf1af18f34e24b0d33d
Vilela <guilhermevilela@hubert.com.br>
2025-07-09 21:19:03 -0300

    feat: route rooms listing

---
+++ server/src/server.ts
@@ -0,0 +1,33 @@
+import { fastifyCors } from "@fastify/cors";
+import fastify from "fastify";
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from "fastify-type-provider-zod";
+import { env } from "./env.ts";
+import { getRoomsRoute } from "./http/routes/get-rooms.ts";
+
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.register(fastifyCors, {
+  origin: "http://localhost:5432",
+});
+
+app.setSerializerCompiler(serializerCompiler);
+
+app.setValidatorCompiler(validatorCompiler);
+
+app.get("/health", () => {
+  return "ok";
+});
+
+app.register(getRoomsRoute);
+
+app
+  .listen({
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log("Server Running!", process.env.PORT);
+  });
