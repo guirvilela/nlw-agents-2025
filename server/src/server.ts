dbb858b668b1b83f7d57a2d700ed263e9f5821d5
Vilela <guilhermevilela@hubert.com.br>
2025-07-11 11:16:28 -0300

    feat(server): create-room

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,6 +6,9 @@ import {
   type ZodTypeProvider,
 } from "fastify-type-provider-zod";
 import { env } from "./env.ts";
+import { createQuestionRoute } from "./http/routes/create-question.ts";
+import { postRoomRoute } from "./http/routes/create-room.ts";
+import { getRoomQuestion } from "./http/routes/get-room-questions.ts";
 import { getRoomsRoute } from "./http/routes/get-rooms.ts";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
@@ -23,6 +26,9 @@ app.get("/health", () => {
 });
 
 app.register(getRoomsRoute);
+app.register(postRoomRoute);
+app.register(getRoomQuestion);
+app.register(createQuestionRoute);
 
 app
   .listen({
